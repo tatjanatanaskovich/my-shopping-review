ff8060fac69ab99de358aef33c841fed0491e9ce
Tatjana Tanaskovic <tatjanatanaskovich@gmail.com>
2017-12-21 11:02:24 +0000

    Add devise and bootstrap for authentication and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   
-  root 'pages#home' # reference is root_path
+  root 'welcome#index' # reference is root_path
   
-  get 'about', to: 'pages#about'
+  #get 'about', to: 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
