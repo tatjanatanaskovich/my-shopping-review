9f0224d11ecbb87622750a7d310cbdf4780adb2c
Tatjana Tanaskovic <tatjanatanaskovich@gmail.com>
2017-12-22 14:01:08 +0000

    Extract redundancy and prepared for production

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :update, :show, :destroy]
   
   def new
     @item = Item.new
@@ -15,15 +16,15 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id]) ovo vise ne treba sada je set_item
   end
   
   def edit
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id]) ovo vise ne treba sada je set_item
   end
   
   def update
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id]) ovo vise ne treba sada je set_item
     if @item.update(item_params)
       flash[:notice] = "Item was successfully updated!"
       redirect_to item_path(@item)
@@ -37,13 +38,17 @@ class ItemsController < ApplicationController
   end
   
   def destroy
-    @item = Item.find(params[:id])
+    #@item = Item.find(params[:id]) ovo vise ne treba sada je set_item
     @item.destroy
     flash[:notice] = "Item was successfully deleted!"
     redirect_to items_path
   end
   
   private
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
   
   def item_params
     params.require(:item).permit(:name, :price, :date)
